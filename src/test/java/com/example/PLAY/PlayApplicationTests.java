7dd1aa9ba905a11eb73928474f2f2a51049c8421
KevinGosling <sonnbunna@gmail.com>
2020-05-30 11:32:34 +0900

    git play

---
+++ src/test/java/com/example/PLAY/PlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PLAY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
